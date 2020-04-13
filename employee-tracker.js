056aa585240a90f794426f689d2a401a080fd9c2
yotamba12 <yotambaram@gmail.com>
2020-04-12 22:40:41 -0700

    finished with add and view futuers

--- employee-tracker.js
+++ employee-tracker.js
@@ -50,7 +50,7 @@ function addData() {
   inquirer
     .prompt({
       type: "list",
-      choices: ["Add department", "Add role title", "Add employee", "Back to menu"],
+      choices: ["Add department", "Add role", "Add employee", "Back to menu"],
       message: "Witch Data Do you want to add?",
       name: "add_data"
     })
@@ -58,7 +58,7 @@ function addData() {
       // based on their answer, either call the bid or the post functions
       if (answer.add_data === "Add department") {
         addNewDepartment();
-      } else if (answer.add_data === "Add role title") {
+      } else if (answer.add_data === "Add role") {
         addNewRole();
       } else if (answer.add_data === "Add employee") {
         addNewEmployeeName();
@@ -70,6 +70,7 @@ function addData() {
     });
 }
 
+////////////////////
 // add new department
 function addNewDepartment() {
   inquirer.prompt({
@@ -87,31 +88,50 @@ function addNewDepartment() {
   })
 }
 
+////////////////////
 // add new role
 function addNewRole() {
+  let departmentsList = getDepartmentsArr()
+  let newRole = []
+  console.log(departmentsList)
   inquirer
-    .prompt(
+    .prompt([
       {
         type: "input",
-        message: "Enter new title",
-        name: "add_title"
+        message: "Enter new role",
+        name: "title",
       },
       {
         type: "input",
-        message: "Enter new title salary",
-        name: "add_title"
+        message: "Enter new role salary",
+        name: "salary",
+      },
+      {
+        type: "list",
+        choices: departmentsList,
+        message: "Choose the roles department",
+        name: "department",
       }
-    ).then(function (answer) {
-      let newTitle = answer.add_title
-      connection.query("INSERT INTO role SET ?",{title: newTitle}
-      ///////////////SALARY//////////////
-      ///////////////choose dep////////
-        , function (err, results) {
-          if (err) throw err;
-          console.log(`${newTitle} add to titles`)
-          start()
+    ]).then(function (answer) {
+      let query = `SELECT department_id FROM department WHERE department.name = 'support'`
+      connection.query(query, function (err, res) {
+          CreateNewRoleDB(answer.title, answer.salary, res[0].department_id);
         })
     })
+}
+
+function CreateNewRoleDB(newTitle, newSalary, departmentID){
+  connection.query("INSERT INTO role SET ?",
+        {
+          title: newTitle,
+          salary: newSalary,
+          department_id: departmentID
+        }
+        , function (err, results) {
+        if (err) throw err;
+        console.log(`added to employees data`);
+        start()
+      });
 }
 
 
@@ -200,7 +220,8 @@ function CreateNewEmployeeDB(){
       });
 }
 
-// get managers names
+//possible to make array builder function with diffirent querys
+// get managers array
 function getManagerArr(){
   const namesArr = [];
   let query = "SELECT * FROM (employee INNER JOIN role ON (employee.role_id = role.role_id)) WHERE (role.title = 'manager')";
@@ -213,6 +234,7 @@ function getManagerArr(){
   });
   return namesArr;
 }
+
 
 // get roles array
 function getTitlesArr(){
@@ -227,6 +249,18 @@ function getTitlesArr(){
   return titleArr;
 }
 
+// get department array
+function getDepartmentsArr(){
+  let departmentArr = [];
+  connection.query("SELECT name FROM department", function (err, res) {
+    if (err) throw err;
+    for (let i = 0; i < res.length; i++) {
+      let e = res[i].name;
+      departmentArr.push(e);
+    }
+  });
+  return departmentArr;
+}
 
 // ************** VIEW DATA **************
 function chooseViewData() {
